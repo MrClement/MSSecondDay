881ae9ca1aa75cef37b0567cdb206e7328253be1
Alex Clement <aclement@kentdenver.org>
2016-01-07 14:26:48 -0700

    Create final driver

---
+++ main.py
@@ -0,0 +1,15 @@
+import ahoff
+import cmalley
+import gmoltz
+import jgreco
+import jkoeplin
+import jparkhouse
+import vramsey
+
+ahoff.do_something()
+cmalley.do_something()
+gmoltz.do_something()
+jgreco.do_something()
+jkoeplin.do_something()
+jparkhouse.do_something()
+vramsey.do_something()
