2f4fd8604924bb9d6fe907c405fb29e07bf222b0
Alex Clement <aclement@kentdenver.org>
2016-01-13 15:19:48 -0700

    Add git ignore

---
+++ jgreco.py
@@ -0,0 +1,6 @@
+def do_something():
+        input = raw_input()
+        print "Type a number: "
+        input = y
+        sum = (y * y)
+        return sum
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import aclement
 import ahoff
 import cmalley
 import gmoltz
-import jgreco
-import jkoeplin
+#import jgreco
+#import jkoeplin
 import jparkhouse
 import vramsey
 
@@ -11,7 +11,7 @@ aclement.do_something()
 ahoff.do_something()
 cmalley.do_something()
 gmoltz.do_something()
-jgreco.do_something()
-jkoeplin.do_something()
+#jgreco.do_something()
+#jkoeplin.do_something()
 jparkhouse.do_something()
 vramsey.do_something()
