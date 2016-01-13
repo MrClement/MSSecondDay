009c7ff4dd1975f30a18095cb354944bf1dbe1c7
Gabriel Moltz <gmoltz21@Vygovoriteporusski-2.local>
2016-01-13 15:07:55 -0700

    Gabusss

--- gmoltz.py
+++ gmoltz.py
@@ -1,6 +1,7 @@
+
 def do_something():
     minions = (raw_input("Should Addison shave his head?"))
-        if minions == "y" or "yes"
+        if minions == "y" or minions == "yes":
             print "Nice me-me gui"
-        if minions == "n" or "no"
+        if minions == "n" or minions == "no":
             print "Your basically Jude"
